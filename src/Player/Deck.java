bb3ba766c5141f19f85e64bce4da06a1066295f3
sarakh99 <sampadsara@gmail.com>
2019-04-20 17:43:59 +0430

    account changes

--- src/Player/Deck.java
+++ src/Player/Deck.java
@@ -1,4 +1,5 @@
 package Player;
 
 public class Deck {
+
 }
--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,4 +1,61 @@
 package Player;
 
+import java.util.ArrayList;
+
 public class Player {
+    protected String username;
+    protected Deck mainDeck;
+}
+
+class Account extends Player {
+    private static ArrayList<Account> accounts = new ArrayList<>();
+    private ArrayList<Deck> decks;
+    private ArrayList<Match> matchHistory;
+    private String password;
+    private int daric;
+    private Collection collection;
+
+    public static ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+    public static void addToAccounts(Account account) {
+        accounts.add(account);
+    }
+
+    public ArrayList<Deck> getDecks() {
+        return decks;
+    }
+
+    public ArrayList<Match> getMatchHistory() {
+        return matchHistory;
+    }
+
+    public void addToMatchHistory(Match match) {
+        matchHistory.add(match);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getDaric() {
+        return daric;
+    }
+
+    public void setDaric(int daric) {
+        this.daric = daric;
+    }
+
+    public void setCollection(Collection collection) {
+        this.collection = collection;
+    }
+
+    public Collection getCollection() {
+        return collection;
+    }
+
 }
