a07d26d030963a483f57c48989b9d79ac1fc80c7
rostaroghani <rrosta@yahoo.com>
2019-04-20 12:24:38 +0430

    shop methods

--- src/Shop/Shop.java
+++ src/Shop/Shop.java
@@ -1,4 +1,17 @@
 package Shop;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Shop {
+    //protected ArrayList<Collective> collection = new ArrayList<Collective>();
+    public void showCollection(){}
+    public int search(String name){}
+    //public Array searchCollection(String name){}
+    public void buy(String name){}
+    public void sell(int ID){}
+    public void show(){}
+    public void showShopHelp(){}
+
+
 }
