fd22ed3d21af954b842659ab5da9d697f46bdbca
rostaroghani <rrosta@yahoo.com>
2019-05-01 13:55:10 +0430

    add shop menu - changes in main

---
+++ src/Shop/ShopMenu.java
@@ -0,0 +1,14 @@
+package Shop;
+
+public class ShopMenu {
+    public void help(){
+        System.out.println("Collection");
+        System.out.println("Shop");
+        System.out.println("Battle");
+        System.out.println("Exit");
+        System.out.println("Help");
+    }
+    public void exit(){
+        return;
+    }
+}
