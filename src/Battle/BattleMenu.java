10fe480432ab49bab48ad347b7fd135aad1aa2d5
Yeganeh Gharedaghi <ygnh@Yeganehs-MacBook-Pro.local>
2019-05-01 18:53:27 +0430

    changes in game

--- src/Battle/BattleMenu.java
+++ src/Battle/BattleMenu.java
@@ -1,13 +1,18 @@
 package Battle;
 
 
+import Collective.Flag;
 import Player.Player;
 
 public class BattleMenu {
     //mode 1 : kill competitor's hero 2: keep the only flag for six turns 3: collect half of flags
     //kind 1 : Story 2: Custom Game
     public void singlePlayerMode(Player player,int kind , int mode){
-        Game game= new Game();
+        Game game = new Game();
         game.setPlayer1(player);
+        game.setMode(mode);
+        if(mode == 2){
+            Flag flag = new Flag();
+        }
     }
 }
