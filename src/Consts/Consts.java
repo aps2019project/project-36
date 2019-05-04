a4caf6d1939697e080ee7ba00067131063cde6f5
sarakh99 <sampadsara@gmail.com>
2019-05-05 04:00:18 +0430

    Completed consts

--- src/Consts/Consts.java
+++ src/Consts/Consts.java
@@ -9,6 +9,34 @@ public class Consts {
     private Card[] cards;
     private Item item;
     private final int initialDaricNum = 500;
+    public static final int MONEY = 1000000;
+    public static final String NONE = "none";
+    public static final String ALL = "all";
+    public static final String PASSIVE = "passive";
+    public static final String CASTABLE = "castable";
+    public static final String FRIEND = "friend";
+    public static final String ENEMY = "enemy";
+    public static final String HERO = "hero";
+    public static final String MINION = "minion";
+    public static final String HERO_MINION = "hero and minions";
+    public static final String WHOLE_UNITS = "whole units";
+    public static final String WHOLE_HEROES = "whole heroes";
+    public static final String WHOLE_MINIONS = "whole minions";
+    public static final String CELL = "cell";
+    public static final String MELEE = "melee";
+    public static final String RANGED = "ranged";
+    public static final String HYBRID = "hybrid";
+    public static final String MELLE_RANGED = "melee and ranged";
+    public static final String RANGED_HYBRID = "ranged and hybrid";
+    public static final String MELEE_HYBRID = "melee and hybrid";
+    public static final String POSITIVE = "positive";
+    public static final String NEGATIVE = "negative";
+    public static final String DISARM = "disarm";
+    public static final String POISON = "poison";
+    public static final String ENEMY_CARD_SPELL = "enemy card spell";
+    public static final String WEAKER_AP = "weaker ap";
+    public static final String HOLY_BUFF = "holy buff";
+
 
     public static Consts getInstance() {
         if(consts == null)
