16033046766362bf8a0a8f6e0905c318b56ce118
Yeganeh Gharedaghi <ygnh@Yeganehs-MacBook-Pro.local>
2019-05-05 17:32:58 +0430

    battle creating

--- src/Battle/BattleMenu.java
+++ src/Battle/BattleMenu.java
@@ -1,8 +1,9 @@
 package Battle;
 
 
-import Collective.Flag;
+import Map.Cell;
 import Player.Player;
+import Menu.Menu;
 
 public class BattleMenu {
     //mode 1 : kill competitor's hero 2: keep the only flag for six turns 3: collect half of flags
@@ -11,8 +12,33 @@ public class BattleMenu {
 
     }
     public void singlePlayerCustom(Player player, int mode, int numOfFlags){
-    }
-    public void multiPlayer(Player player1, Player player2, int mode, int numOfFlags){
 
+    }
+
+    public void multiPlayer(Player player1, Player player2, int mode, int numOfFlags){
+        Battle battle = new Battle();
+        Game game = new Game();
+        Cell cell = new Cell();
+        cell.setX(0);
+        cell.setY(3);
+        game.setPlayer1(player1);
+        game.setPlayer2(player2);
+        game.setManaPlayer1(2);
+        game.setManaPlayer2(3);
+        player1.getMainDeck().getHero().setCell(cell);
+        cell.setX(8);
+        player2.getMainDeck().getHero().setCell(cell);
+        game.setMode(mode);
+        while(game.checkIsOver() == 0){
+            battle.gameInfo();
+            String input = Menu.getInput();
+            if (input.compareToIgnoreCase("") == 0){
+
+            }
+            battle.playTurn();
+            game.setMana(1);
+            game.setMana(2);
+            game.changeTurn();
+        }
     }
 }
