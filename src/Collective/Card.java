8cfc77785125599b2f69e8499b5b3b84d5027753
Yeganeh Gharedaghi <ygnh@Yeganehs-MacBook-Pro.local>
2019-05-07 12:09:54 +0430

    addToCards

--- src/Collective/Card.java
+++ src/Collective/Card.java
@@ -43,7 +43,7 @@ public abstract class Card {
         return cards;
     }
 
-    public static void addCards(Card card) {
+    public static void addToCards(Card card) {
         cards.add(card);
     }
 
