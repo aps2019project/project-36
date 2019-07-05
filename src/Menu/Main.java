67a4ecb9a44c71c1c576a77a21c17d60a9c320e5
rostaroghani <rrosta@yahoo.com>
2019-07-05 21:16:28 +0430

    try catch

--- src/Menu/Main.java
+++ src/Menu/Main.java
@@ -2,13 +2,19 @@ package Menu;
 
 import Shop.Shop;
 
+import java.io.IOException;
+
 public class Main {
 
 
     public static void main(String[] args) {
         Shop shop=new Shop();
         Menu.makeAllAccounts ();
-        Graphics.graphics(args);
+        try {
+            Graphics.graphics(args);
+        } catch (IOException e) {
+            e.printStackTrace ( );
+        }
     }
 
     /*public static void main(String[] args) {
